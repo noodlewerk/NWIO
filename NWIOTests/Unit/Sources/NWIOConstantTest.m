08bea1a262730be0cab8baba2bb4b34ac6d4d7a1
Leonard van Driel <leonardvandriel@gmail.com>
2011-11-16 23:05:44 +0100

    added issue test constant

---
+++ NWIOTests/Unit/Sources/NWIOConstantTest.m
@@ -0,0 +1,33 @@
+//
+//  NWIOConstantTest.m
+//  NWIO
+//
+//  Copyright 2011 Noodlewerk
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "NWIOConstantTest.h"
+#import "NWIO.h"
+
+
+@implementation NWIOConstantTest
+
+- (void)test {
+    NWIOConstantAccess *constant = [[NWIOConstantAccess alloc] initWithInputLength:10 outputLength:10];
+    const void *buffer = nil;
+    NSUInteger length = [constant readable:&buffer location:0];
+    NSAssert(length, @"Should read something");
+}
+
+@end
